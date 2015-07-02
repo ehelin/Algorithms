ec7b2f6385ef6a88a6aa8ad11da0f83ee5a68f90
Eric Helin <ehelin@felpsis.net>
2015-07-01 22:42:49 -0500

    Initial...

---
+++ Algorithms/Genetic/Algorithm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Genetic
+{
+    public class Algorithm
+    {
+        public Algorithm()
+        {
+
+        }
+        
+        public void Run()
+        {
+            Console.WriteLine("Starting Genetric Algoritm Example");
+
+            RunGeneticAlgorithm();
+
+            Console.WriteLine("Genetric Algoritm Completed!");
+        }
+
+        private void RunGeneticAlgorithm()
+        {
+
+        }
+    }
+}
---
+++ SoftwareAlgorithms/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Algorithms.Genetic;
+
+namespace SoftwareAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Algorithms.Main aM = new Algorithms.Main();
+            aM.Run();
+        }
+    }
+}
